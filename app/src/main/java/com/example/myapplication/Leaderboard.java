e7b6ac835969c95758e1e5bb0917b2d883472295
ciodya <1502882191@qq.com>
2019-03-11 18:31:33 +0000

    first commit

---
+++ app/src/main/java/com/example/myapplication/Leaderboard.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Leaderboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Prize.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class Prize extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        TextView title = (TextView) findViewById(R.id.title);
+        setContentView(R.layout.activity_prize);
+
+    }
+}
